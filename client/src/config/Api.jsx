fd26d99398e59fcd8b5129f6e0e8ce40a600218b
arpit168 <arpitgupta0406@gmail.com>
2026-01-20 19:07:23 +0530

    Update axios instance baseURL to use production endpoint

--- client/src/config/Api.jsx
+++ client/src/config/Api.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     // baseURL:"http://localhost:4500",
-    baseURL:"http://192.168.0.141:4500",
+    baseURL:"https://cravings-food.onrender.com",
     withCredentials:true,
 })
 
