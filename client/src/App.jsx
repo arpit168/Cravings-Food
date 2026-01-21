1888747686eafd7f69085d29b236d74d86b274bb
arpit168 <arpitgupta0406@gmail.com>
2026-01-21 16:03:39 +0530

    improve

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -27,6 +27,7 @@ const App = () => {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/userDashboard" element={<UserDashboard/>}/>
+          <Route path="*" element={<NotFound />} />
         </Routes>
         <Footer />
       </BrowserRouter>
