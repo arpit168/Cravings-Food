ec2dd656f1a09f482d59f6be76bf9978b15c2fa3
arpit168 <arpitgupta0406@gmail.com>
2026-01-21 17:26:02 +0530

    ..

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,19 +7,17 @@ import Contact from "./pages/Contact";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Footer from "./components/Footer";
-import NotFound from "./pages/Error"
+import NotFound from "./pages/Error";
 import { Toaster } from "react-hot-toast";
 import UserDashboard from "./pages/dashboards/UserDashboard";
 import useUiStore from "./stores/useUiStore";
 
-  
-
-const App = () => { 
-  const {setShowHeader} = useUiStore();
+const App = () => {
+  const { setShowHeader } = useUiStore();
   return (
-    <div onClick={()=>setShowHeader(false)}>
+    <div onClick={() => setShowHeader(false)}>
       <BrowserRouter>
-      <Toaster/>
+        <Toaster />
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
@@ -27,7 +25,7 @@ const App = () => {
           <Route path="/contact" element={<Contact />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/userDashboard" element={<UserDashboard/>}/>
+          <Route path="/userDashboard" element={<UserDashboard />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
         <Footer />
